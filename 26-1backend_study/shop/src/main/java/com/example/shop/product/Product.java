37c179037844ea2e81386abd669c4f524dc90a74
jhon0147 <98521061+jhon0147@users.noreply.github.com>
2026-05-09 23:15:28 +0900

    product 업뎃

--- 26-1backend_study/shop/src/main/java/com/example/shop/product/Product.java
+++ 26-1backend_study/shop/src/main/java/com/example/shop/product/Product.java
@@ -1,14 +1,12 @@
 package com.example.shop.product;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "products")
 @Getter
 @Setter
 @NoArgsConstructor
